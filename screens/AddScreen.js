b8306cccdd509388082d03d6cb4ec2d63c5561ec
Jack Lim <wjie_lim@outlook.com>
2024-08-28 15:38:18 +0800

    Test git pull upstream for student

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,6 +1,8 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+console.log("test");
+
 const AddScreen = () => {
   return (
     <SafeAreaView>
